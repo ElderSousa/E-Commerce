de36d12169162f282dd950f4e66b3379592423ae
francis elder de sousa batista <elder2faces@gmail.com>
2025-11-14 15:31:41 -0300

    Configuração swagger concluída.

--- src/main/java/com/sprint/ecommerce/config/SpringDocConfig.java
+++ src/main/java/com/sprint/ecommerce/config/SpringDocConfig.java
@@ -2,12 +2,15 @@ package com.sprint.ecommerce.config;
 
 import org.springframework.context.annotation.Bean;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+
 public class SpringDocConfig {
      @Bean
     public OpenAPI openAPI() {
         return new OpenAPI()
-                .info(new Info().title("BookStore - Sistema de gerenciamento de livros")
-                        .description("API REST para gerenciamento livros.")
+                .info(new Info().title("E-commerce - Sistema de E-commerce")
+                        .description("API REST para gerenciamento de E-commerce.")
                         .version("v1.0.0"));
     }
 }
