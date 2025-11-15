49d1e0e78f7acbcfba35d9e9ed6980bbbf4a3532
francis elder de sousa batista <elder2faces@gmail.com>
2025-11-15 12:07:46 -0300

    Mapeamento da entitdades com jpa concluído

--- src/main/java/com/sprint/ecommerce/models/Categoria.java
+++ src/main/java/com/sprint/ecommerce/models/Categoria.java
@@ -1,12 +1,18 @@
 package com.sprint.ecommerce.models;
 
-import java.util.UUID;
+import java.util.HashSet;
+import java.util.Set;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.SequenceGenerator;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -23,9 +29,15 @@ import lombok.Setter;
 @Builder
 public class Categoria {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private UUID id;
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "categoria_seq")
+    @SequenceGenerator(name = "categoria_seq", sequenceName = "categoria_seq", allocationSize = 1)
+    private Long id;
     @Column(nullable = false, unique = true)
     private String nome;
+
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    @OneToMany(mappedBy = "categoria", cascade = CascadeType.ALL, orphanRemoval = true) 
+    @Builder.Default //Garante que o builder inicialize a coleção.
+    private Set<Produto> produtos = new HashSet<>();
 
 }
